4b590476fb0944d461cb16decc0391411303dfb5
Alec Tower <alectower@gmail.com>
2016-05-30 14:24:57 -0400

    Update sync supervisor to include new workers

--- lib/sync.ex
+++ lib/sync.ex
@@ -2,6 +2,8 @@ defmodule Ledger.Sync do
   use Supervisor
   alias Ledger.Sync.Bank
   alias Ledger.Sync.TradeKing
+  alias Ledger.Sync.Scottrade
+  alias Ledger.Sync.Vanguard
 
   def start_link do
     Supervisor.start_link(__MODULE__, [])
@@ -10,16 +12,22 @@ defmodule Ledger.Sync do
   def init(arg) do
     children = [
       worker(Bank, []),
-      worker(TradeKing, [])
+      worker(TradeKing, []),
+      worker(Scottrade, []),
+      worker(Vanguard, [])
     ]
 
     supervise(children, strategy: :one_for_one)
   end
 
   def run do
-    bank = Task.async &Bank.fetch_data/0
-    trade = Task.async &TradeKing.fetch_data/0
-    Task.await(bank, 10000000)
-    Task.await(trade, 10000000)
+   [
+     &Bank.fetch_data/0,
+     &TradeKing.fetch_data/0,
+     &Scottrade.fetch_data/0,
+     &Vanguard.fetch_data/0
+   ]
+   |> Enum.map(&(Task.async &1))
+   |> Enum.map(&(Task.await &1, 10000000))
   end
 end
