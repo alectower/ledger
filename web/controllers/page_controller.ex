f5fb301c63dfaab8221656fe15363c49c0cf3619
Alec Tower <alectower@gmail.com>
2016-05-30 14:25:07 -0400

    Order accounts by name

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,8 +4,8 @@ defmodule Ledger.PageController do
 
   def index(conn, _params) do
     conn
-    |> assign(:assets, Repo.all(from(a in Account, where: a.type == 0)))
-    |> assign(:liabilities, Repo.all(from(a in Account, where: a.type == 1)))
+    |> assign(:assets, Repo.all(from(a in Account, where: a.type == 0, order_by: [a.name])))
+    |> assign(:liabilities, Repo.all(from(a in Account, where: a.type == 1, order_by: [a.name])))
     |> render "index.html"
   end
 end
