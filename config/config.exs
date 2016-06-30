0844feb85fcaa7e95c1a3962b221a31781713b7a
Alec Tower <alectower@gmail.com>
2016-06-30 19:35:04 -0400

    Reset hound browser config

--- config/config.exs
+++ config/config.exs
@@ -22,7 +22,7 @@ config :logger, :console,
 config :phoenix, :template_engines,
   haml: PhoenixHaml.Engine
 
-config :hound, driver: "phantomjs", port: 8080, http: [connect_timeout: 1000000, timeout: 1000000, recv_timeout: 1000000]
+config :hound, browser: "chrome", port: 8080, http: [connect_timeout: 1000000, timeout: 1000000, recv_timeout: 1000000]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
